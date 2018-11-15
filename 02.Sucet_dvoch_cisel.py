74295f3af82e71e8f87ee172bcb33cb57d50498f
Evanjelik <andrejdudasss@gmail.com>
2018-11-15 21:10:40 +0100

    hotofka

--- 02.Sucet_dvoch_cisel.py
+++ 02.Sucet_dvoch_cisel.py
@@ -1,1 +1,5 @@
-
+print("Budeme pocitat. Zadas dve cisla a ukazem ti vysledok.")
+prve_cislo = input("Zadaj prve cislo: ")
+druhe_cislo = input("Zadaj druhe cislo: ")
+vysledok = float(prve_cislo) + float(druhe_cislo)
+print("Vysledok je " + str(vysledok))
