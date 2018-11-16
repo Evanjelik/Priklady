d099f1ddf6236ed8c86d7636f43e0a6e4a9eb005
Evanjelik <andrejdudasss@gmail.com>
2018-11-16 18:04:36 +0100

    hotofka. Dal som to ale vdaka googleniu

--- 02.Sucet_dvoch_cisel.py
+++ 02.Sucet_dvoch_cisel.py
@@ -1,5 +1,9 @@
 print("Budeme pocitat. Zadas dve cisla a ukazem ti vysledok.")
 prve_cislo = input("Zadaj prve cislo: ")
+while not prve_cislo:
+    prve_cislo = input("Zadaj prve cislo: ")
 druhe_cislo = input("Zadaj druhe cislo: ")
+while not druhe_cislo:
+    druhe_cislo = input("Zadaj druhe cislo: ")
 vysledok = float(prve_cislo) + float(druhe_cislo)
 print("Vysledok je " + str(vysledok))
